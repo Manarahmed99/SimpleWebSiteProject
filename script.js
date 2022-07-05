7127254cc881bab5d74017fc4918165a5fa0552c
Manarahmed99 <62827972+Manarahmed99@users.noreply.github.com>
2022-07-05 13:56:07 +0300

    Art-Angle-Galleary

---
+++ script.js
@@ -0,0 +1,16 @@
+
+        function validate() {
+                var email = document.getElementById("email").value;
+                var password = document.getElementById("password").value;
+                if (email== "") {
+                    alert("Please enter your email.");
+                    return false;
+                }
+                if (password == "") {
+                    alert("Please enter your password.");
+                    return false;
+                }
+                alert('Login successful');
+                
+            }
+
